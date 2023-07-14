fe9f9f95c296073cd7e5c2207d9a29b658eff302
Light DAO Weaver <lightweaver1998@gmail.com>
2023-07-14 11:02:38 -0700

    updated

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 		printf("%d and is greater than 5\n", lastDigit);
 	else if (lastDigit == 0)
 		printf("%d and is 0\n", lastDigit);
-	else if (lastDigit < 6 && lastDigit !== 0)
+	else if (lastDigit < 6 && lastDigit != 0)
 		printf("%d and is less than 6 and not 0\n", lastDigit);
 
 	return (0);
