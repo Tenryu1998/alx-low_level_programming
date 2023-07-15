5e4c731da957224336e5a44edf1e373fcc7238e4
Light DAO Weaver <lightweaver1998@gmail.com>
2023-07-15 04:30:51 -0700

    updated

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	int number;
+	char letter;
 
-	for (number = 0; number < 10; number++)
-		putchar(number + '0');
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
 
 	putchar('\n');
 
