d6f899892c7a28495cc0f608b527940bbb10833c
Light DAO Weaver <lightweaver1998@gmail.com>
2023-07-15 06:34:47 -0700

    updated

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the string "and that piece of art is useful"
+ * 		 - Dora Korpar, 2015-10-19,"
+ *              followed by a newline character, to the standard error.
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
