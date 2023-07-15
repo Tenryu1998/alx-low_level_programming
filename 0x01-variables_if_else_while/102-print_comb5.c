fe8a6c0e5e42fccef596cc9df3d13eae558eb5cf
Light DAO Weaver <lightweaver1998@gmail.com>
2023-07-15 05:06:12 -0700

    updated

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,9 +10,9 @@ int main(void)
 	int num1;
 	int num2;
 
-	for (num1 = 0; num1 < 100; num1++)
+	for (num1 = 0; num1 <= 99; num1++)
 	{
-		for (num2 = num1; num2 < 100; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
@@ -20,7 +20,7 @@ int main(void)
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 
-			if (num1 != 99 || num2 != 99)
+			if (num1 != 98 || num2 != 99)
 			{
 				putchar(',');
 				putchar(' ');
@@ -32,4 +32,3 @@ int main(void)
 
 	return (0);
 }
-
