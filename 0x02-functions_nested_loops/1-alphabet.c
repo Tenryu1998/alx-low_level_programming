bd78c9a0365db6fba71d05a16e885d8880d5c89f
Light DAO Weaver <lightweaver1998@gmail.com>
2023-07-19 11:58:56 -0700

    updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,4 @@
-#include <stdio.h>
 #include "main.h"
-
-/**
- * main - Entry point of the program
- *
- * Description: Call print_alphabet
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 
 /**
  * print_alphabet - Prints the alphabet in lowercase followed by a new line.
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_HEADER
-#define MAIN_HEADER
+#ifndef MAIN_H
+#define MAIN_H
 
 void print_alphabet(void);
 
