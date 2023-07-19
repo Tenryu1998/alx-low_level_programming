65a39774d8fdd3b4bae76782c690c3ec7813003f
Light DAO Weaver <lightweaver1998@gmail.com>
2023-07-19 10:16:46 -0700

    updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the string "_putchar" followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	printf("_putchar\n");
-	return 0;
+	return (0);
 }
