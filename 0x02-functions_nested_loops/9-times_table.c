b4aee5b3b2a0f42bf6d699b935eaf99a4266a3d8
Light DAO Weaver <lightweaver1998@gmail.com>
2023-07-19 14:23:42 -0700

    updated

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,7 @@ void times_table(void)
 
 			if (column > 0 &&  product < 10)
 				_putchar(' ');
-			else
+			else if (product >= 10)
 				_putchar((product / 10) + '0');
 
 			_putchar((product % 10) + '0');
