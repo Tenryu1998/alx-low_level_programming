73d6f131d5ca60fea6d04dad6f1dbe31122372f6
Light DAO Weaver <lightweaver1998@gmail.com>
2023-07-19 12:29:48 -0700

    updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet_x10 - Prints the alphabet in lowercase
- * 			ten times followed by a new line.
+ *			ten times followed by a new line.
  */
 void print_alphabet_x10(void)
 {
