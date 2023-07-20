179c7a83e4bd586ffe9bddfdbddc7a98ada6cef5
Light DAO Weaver <lightweaver1998@gmail.com>
2023-07-20 06:33:44 -0700

    updated

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,33 +1,28 @@
 #include "main.h"
 
 /**
- * print_diagonal - Draws a diagonal line in the terminal using the character '\'.
- * @n: The number of times the character '\' should be printed.
- *
- * Description: This function prints the character '\' n times in a diagonal pattern
- *              and ends the diagonal with a newline character '\n'. If n is 0 or less,
- *              it prints only the newline character to create a blank line.
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character \ should be printed
  */
 void print_diagonal(int n)
 {
-	int spaces;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
-		return;
-	}
-
-	while (n > 0)
+	} else
 	{
-		spaces = n - 1;
-		while (spaces > 0)
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
-			spaces--;
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
 		}
-		_putchar('\\');
-		_putchar('\n');
-		n--;
 	}
 }
