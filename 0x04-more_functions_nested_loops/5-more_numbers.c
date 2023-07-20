44ab8e23645019598d0419adba485679d05e5303
Light DAO Weaver <lightweaver1998@gmail.com>
2023-07-20 06:00:10 -0700

    updated

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints 10 times the numbers from 0 to 14, followed by a new line.
+ *
+ * Description: This function prints the numbers 0 to 14, each repeated 10 times,
+ *              and then followed by a new line. It uses the _putchar function
+ *              to print the characters to the standard output.
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
