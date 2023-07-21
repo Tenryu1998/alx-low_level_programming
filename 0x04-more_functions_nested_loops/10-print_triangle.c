c49787d5f88b254d1db13478188438afac79a72d
tenryu1998 <lightweaver1998@gmail.com>
2023-07-21 09:10:32 -0700

    added 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -18,17 +18,19 @@ void print_triangle(int size)
 	if (size <= 0)
 	{
 		_putchar('\n');
-		return();
 	}
-
-	for (i = 1; i <= size; i++)
+	else
 	{
-		for (j = 0; j < size - i; j++)
-			_putchar(' ');
 
-		 for (j = 0; j < i; j++)
-			_putchar('#');
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 0; j < size - i; j++)
+				_putchar(' ');
 
-		_putchar('\n');
+		 	for (j = 0; j < i; j++)
+				_putchar('#');
+
+			_putchar('\n');
+		}
 	}
 }
