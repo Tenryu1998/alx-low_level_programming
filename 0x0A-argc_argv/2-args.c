6c5fa6b381375d8d8e5b8a9ebeee6507ff189056
tenryu1998 <lightweaver1998@gmail.com>
2023-08-08 06:22:09 -0700

    fixed bug in task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,10 +7,12 @@
 *Return: returns 0
 */
 
-int main(int argc, char *argv[]) {
-	for (int i = 0; i < argc; i++) {
-		printf("%s\n", argv[i]);
-	}
-	return 0;
-}
+int main(int argc, char *argv[])
+{
+		int i;
 
+		for (i = 0; i < argc; i++)
+			printf("%s\n", argv[i]);
+
+		return (0);
+}
