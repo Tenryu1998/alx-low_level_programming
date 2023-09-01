af861d108eb8f0159f3e1ece63ae3cc902ea9ee1
tenryu1998 <lightweaver1998@gmail.com>
2023-08-31 17:38:47 -0700

    fixed bugs

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * is_palindrome - checks if a string is a palindrome
@@ -9,15 +10,15 @@
 int is_palindrome(char *s)
 {
 	int res;
-	int strlen;
+	int len;
 
 	if (*s == 0)
 	{
 		return (1);
 	}
 
-	strlen = strlen_recursor(s);
-	res = palindrome_recursor(s, 0, strlen);
+	len = strlen(s) - 1;
+	res = palindrome_recursor(s, 0, len);
 
 	return (res);
 }
@@ -54,51 +55,18 @@ int strlen_recursor(char *s)
 /**
  * palindrome_recursor - checks the characters recursively for palindrome
  * @s: string to check
- * @i: iterator
+ * @i: index
  * @l: length of the string
  *
  * Return: 1 if palindrome else 0
  */
 int palindrome_recursor(char *s, int i, int l)
 {
-	/**
-	 * ni: the next iterator
-	 * pl: reduced length of string
-	 */
-	int ni = i + 1;
-	int pl = l - 1;
-
-	/**
-	 * si: pointer to the next string character in forward
-	 * sl: pointer to the next string in reverse
-	 */
-	char *si = (s + i);
-	char *sl = (s + pl);
-
-	int res;
-
-	/**
-	 * if the next string character forward
-	 * is not equal to the next string backward
-	 * return 0
-	 * else if i is greater than or equal to l
-	 * that is if the iterator is at the middle or past the middle of the string
-	 * return 1
-	 */
-	if (si != sl)
-	{
-		return (0);
-	}
-	else if (i >= l)
-	{
+	if (i >= l)
 		return (1);
-	}
+    
+	if (s[i] != s[l])
+		return (0);
 
-	/**
-	 * res: calling this function again
-	 * but with the next iterator and the reduced length
-	 */
-	res = palindrome_recursor(s, ni, pl);
-
-	return (res);
+	return palindrome_recursor(s, i + 1, l - 1);
 }
