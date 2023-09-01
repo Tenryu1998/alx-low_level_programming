3794b85daf8f0f84ed14bbdf3c9f09f6526e7a60
tenryu1998 <lightweaver1998@gmail.com>
2023-08-31 19:38:58 -0700

    updated script

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,13 +10,38 @@
 
 int wildcmp(char *s1, char *s2)
 {
+	int res = 0;
+
+	/**
+	 * if s2 is empty return
+	 * the result is the condition: is s1 also empty
+	 */
 	if (*s2 == '\0')
-		return (*s1 == '\0');
+		res = (*s1 == '\0');
 
-	if (*s2 == *s1)
-		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
+	/**
+	 * if s1 and s2 are equal and s1 should not be empty
+	 * the result is the result of calling this same
+	 * function with pointers to the next characters
+	 * in both strings
+	 */
+	if (*s2 == *s1 && *s1 != '\0')
+	{
+		res = wildcmp(s1 + 1, s2 + 1);
+	}
 
+	/**
+	 * s2 is *
+	 */
 	if (*s2 == '*')
-		return ((wildcmp(s1, s2 + 1)) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
-	return (0);
+	{
+		res = wildcmp(s1, s2 + 1);
+
+		if (res == 0)
+		{
+			if (*s1 != '\0')
+				res = wildcmp(s1 + 1, s2);
+		}
+	}
+	return (res);
 }
