81c091bd8e8dbf3ec5801d5c37f72872c8c0fd90
tenryu1998 <lightweaver1998@gmail.com>
2023-09-02 02:06:37 -0700

    fixed extra bugs

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,14 +13,17 @@ int main(int argc, char *argv[])
 {
 	int sum = 0;
 	int i = 1;
+	int j;
 
 	for ( ; i < argc; i++)
 	{
-
-		if (*argv[i] < '0' || *argv[i] > '9')
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 
 		sum = sum + atoi(argv[i]);
